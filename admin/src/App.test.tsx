74cc75c8e59922819e33853e024cb1b0e8f82a69
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-03-22 19:58:06 +0530

    react typescript setup with jest testing in admin

---
+++ admin/src/App.test.tsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import {act} from 'react-dom/test-utils';
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+describe('App', function () {
+   it('should display pass in number', function () {
+      //  let container = document.createElement('div');
+      //  document.body.appendChild(container);
+      //  act(() => {
+      //      ReactDOM.render(<App />, container);
+      //  })
+      //  const header = container.querySelector('a');
+      //  console.log(header)
+      //  expect(header).toBe("Learn React")
+   });
+});
