24a6fa18d4eafd1e4a14969abb36cb9a48eaf2e4
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-12-20 10:04:17 +0530

    major changes

--- clients/service/API.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-// create axios instance with base url of api service 
-const API = axios.create({
-    baseURL: process.env.NODE_ENV==='production' ? process.env.API_URL_PROD : process.env.API_URL_DEV,
-})
-export default API
